9c85de4e5542dab584bf1a064cef1aa1a121e41c
Joel García Riera <jgarcia@ADOST.local>
2021-01-11 08:51:54 +0100

    + Test commit

--- app/src/main/java/net/offshoretech/garcitech/ToDoApplication.kt
+++ app/src/main/java/net/offshoretech/garcitech/ToDoApplication.kt
@@ -3,6 +3,7 @@ package net.offshoretech.garcitech
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+//para inyectar
 @HiltAndroidApp
 class ToDoApplication : Application() {
 }
